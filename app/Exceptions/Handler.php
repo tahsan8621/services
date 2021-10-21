600d770f9ceee0613326347fc3630fcfbeb52e72
Tanjeeb Ahsan <tanjeeb@werit.xyz>
2021-10-21 15:36:29 +0600

    fixed debug mode

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,9 +49,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if (env('APP_DEBUG')) {
-            dd($exception);
-        }
         return parent::render($request, $exception);
     }
 }
